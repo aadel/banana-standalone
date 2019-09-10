62d353c1e25e04d3d2e3a4a0186aa1b4dd6f0873
Ahmed Adel <github@aadel.io>
2019-09-10 22:48:28 +0200

    Introducing Solr URL module

---
+++ modules/solrUrl.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+var solrUrl;
+
+if (process.env.BANANA_SOLR_HOST)
+    solrUrl = 'http://' + process.env.BANANA_SOLR_HOST + ':8983';
+else if (config.solrUrl)
+    solrUrl = config.solrUrl;
+else
+    solrUrl = 'http://localhost:8983';
+
+console.log('Solr URL: %s', solrUrl)
+
+module.exports = solrUrl;
