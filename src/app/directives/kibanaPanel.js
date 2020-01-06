a21789bee5b60045b0b59f1deab7f080d628a67d
Ahmed Adel <github@aadel.io>
2020-01-06 12:16:10 +0200

    Fixing spinner alignment

--- src/app/directives/kibanaPanel.js
+++ src/app/directives/kibanaPanel.js
@@ -78,7 +78,7 @@ function (angular) {
 
           '<span class="row-button extra" ng-show="panelMeta.loading == true">' +
             '<span>'+
-              '<i class="icon-spinner smaller icon-spin icon-large"></i>' +
+              '<i class="icon-spinner icon-spin icon-large"></i>' +
             '</span>'+
           '</span>' +
 
