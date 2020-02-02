496711a98c0f1cc8bb99579ce2741167b6d824be
Ahmed Adel <github@aadel.io>
2020-02-02 04:26:34 -0800

    Adding a version directive

---
+++ src/app/directives/banana-directives.js
@@ -0,0 +1,10 @@
+'use strict';
+
+define(['angular', 'config'], (angular, config) => {
+    angular.module('kibana.directives')
+        .directive('appVersion', () => {
+            return function(scope, elm, attrs) {
+                elm.text(config.REV);
+            };
+        });
+});
